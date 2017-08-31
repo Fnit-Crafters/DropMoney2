9bfb32339bbd92d4553a3765592b4b3d06df07e1
TaikiFnit <taikifnitspg@gmail.com>
2017-08-31 23:56:44 +0900

    Release v1.0.0

---
+++ src/main/java/fnitcrafters/dropmoney/DropMoney.kt
@@ -0,0 +1,38 @@
+package fnitcrafters.dropmoney
+
+import net.milkbowl.vault.economy.Economy
+import org.bukkit.event.Listener
+import org.bukkit.plugin.RegisteredServiceProvider
+import org.bukkit.plugin.java.JavaPlugin
+
+class DropMoney : JavaPlugin() {
+
+    private var econ: Economy? = null
+
+    override fun onEnable() {
+        // Plugin startup logic
+        if (!setupEconomy()) {
+            logger.info("Cannot enable Vault")
+            server.pluginManager.disablePlugin(this)
+        }
+
+        this.getCommand("fdm").executor = FdmCommand()
+        server.pluginManager.registerEvents(FdmListeners(econ), this)
+    }
+
+    private fun setupEconomy() : Boolean {
+        if (server.pluginManager.getPlugin("Vault") == null) {
+            return false
+        }
+
+        val rsp = server.servicesManager.getRegistration(Economy::class.java)
+
+        this.econ = rsp.provider
+
+        return econ != null
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/fnitcrafters/dropmoney/FdmListeners.kt
@@ -0,0 +1,45 @@
+@file:Suppress("DEPRECATION")
+
+package fnitcrafters.dropmoney
+
+import net.milkbowl.vault.economy.Economy
+import net.milkbowl.vault.economy.EconomyResponse
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.plugin.RegisteredServiceProvider
+
+
+/**
+ * Created by taikifnit on 2017/08/31.
+ */
+class FdmListeners(econ: Economy?) : Listener {
+
+    var econ: Economy? = null
+
+    init {
+        this.econ = econ
+    }
+
+    @EventHandler
+    fun onEntityDeath(event: EntityDeathEvent?) {
+
+        val econ = econ ?: return
+        val event = event ?: return
+        val entity = event.entity ?: return
+
+        val killer = entity.killer
+
+//        killer.sendMessage("You destoryed " + entity?.name)
+//        killer.sendMessage(econ?.getBalance(killer.name).toString())
+
+        val earnedMoney = event.droppedExp
+        val r = econ.depositPlayer(killer.name, earnedMoney.toDouble())
+
+        if(r.transactionSuccess()) {
+            killer.sendMessage("You've earned " + econ.format(earnedMoney.toDouble()) + " from " + entity.name)
+        }
+    }
+}
